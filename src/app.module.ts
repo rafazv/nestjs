e9f6e39b922bcef35015a0e01a8f155c46f324f2
Rafaela <rafaela.lima@indt.org.br>
2021-12-28 14:25:15 -0400

    Add files

---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { Produto } from './produto.model';
+import { ProdutosController } from './produtos.controller';
+import { ProdutosService } from './produtos.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: 'livraria',
+      autoLoadModels: true,
+      synchronize: true
+    }),
+    SequelizeModule.forFeature([Produto])
+  ],
+  controllers: [AppController, ProdutosController],
+  providers: [AppService, ProdutosService],
+})
+export class AppModule {}
---
+++ src/produtos.controller.ts
@@ -0,0 +1,33 @@
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import { Produto } from './produto.model';
+import { ProdutosService } from "./produtos.service";
+
+@Controller('produtos')
+export class ProdutosController {
+    constructor(private produtosService: ProdutosService) {}
+
+    @Get()
+    async obterTodos(): Promise<Produto[]> {
+        return this.produtosService.obterTodos();
+    }
+
+    @Get(':id')
+    async obterUm(@Param() params): Promise<Produto> {
+        return this.produtosService.obterUm(params.id);
+    }
+
+    @Post()
+    criar(@Body() produto: Produto) {
+        this.produtosService.criar(produto);
+    }
+
+    @Put()
+    async alterar(@Body() produto: Produto): Promise<[number, Produto[]]> {
+        return this.produtosService.alterar(produto);
+    }
+
+    @Delete(':id')
+    async apagar(@Param() params) {
+        this.produtosService.apagar(params.id);
+    }
+}
